b1cdbd1778a8bd5140a1fea6dbd7faa605bad550
Radek <95048257+RadekStaneczek@users.noreply.github.com>
2024-10-02 13:57:59 +0200

    Added VievTouchHelper class

---
+++ app/src/main/java/com/example/todolist/ViewTouchHelper.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewTouchHelper extends ItemTouchHelper.SimpleCallback{
+    private Task_RecyclerViewAdapter adapter;
+    public ViewTouchHelper(Task_RecyclerViewAdapter adapter)
+    {
+        super(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+}
